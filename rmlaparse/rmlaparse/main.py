a4fee0a48f2ded42d8bdfada0545da5bb0386806
Reuven Lerner <reuven@lerner.co.il>
2021-10-17 21:26:47 +0300

    gitautosync update 160

--- rmlaparse/rmlaparse/main.py
+++ rmlaparse/rmlaparse/main.py
@@ -6,7 +6,3 @@ def run():
         '/Users/reuven/Courses/Current/lerner-2021-10oct-17/rmlaparse/tests/access.log.1')
     for one_record in ap.record_dicts():
         print(one_record)
-
-
-if __name__ == '__main__':
-    run()

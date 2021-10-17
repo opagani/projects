64d4a7c8925753dbae8c66af67d03d8f6355810a
Reuven Lerner <reuven@lerner.co.il>
2021-10-17 20:29:08 +0300

    gitautosync update 80

---
+++ rmlaparse/tests/flycheck_test_rmlaparse.py
@@ -0,0 +1,36 @@
+import pytest
+from rmlaparse import __version__
+from rmlaparse.aparser import ApacheParse
+
+
+def test_version():
+    assert __version__ == '0.1.0'
+
+
+# create a fixture with an instance of ApacheParse
+@pytest.mark.fixture
+def 
+
+def test_create_instance():
+    ap = ApacheParse(
+        '/Users/reuven/Courses/Current/lerner-2021-10oct-17/rmlaparse/tests/access.log.1')
+    assert isinstance(ap, ApacheParse)
+
+    # should get back a generator
+    g = ap.parse_file()
+    assert hasattr(g, '__iter__')
+    assert hasattr(g, '__next__')
+
+
+def test_parse_file_returns_list_of_strings():
+    ap = ApacheParse(
+        '/Users/reuven/Courses/Current/lerner-2021-10oct-17/rmlaparse/tests/access.log.1')
+
+    all_records = list(ap.parse_file())
+
+    assert all(isinstance(one_record, list)
+               for one_record in all_records)
+
+    first_record = all_records[0]
+    assert all(isinstance(one_field, str)
+               for one_field in first_record)

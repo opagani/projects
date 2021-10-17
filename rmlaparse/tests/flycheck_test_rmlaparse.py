4c9dcd9ae42c826ce257b1721c605bf80eec4b37
Reuven Lerner <reuven@lerner.co.il>
2021-10-17 20:34:34 +0300

    gitautosync update 91

--- rmlaparse/tests/flycheck_test_rmlaparse.py
+++ rmlaparse/tests/flycheck_test_rmlaparse.py
@@ -32,7 +32,7 @@ def test_parse_file_returns_list_of_strings(apache_parser):
     assert all(isinstance(one_record, list)
                for one_record in all_records)
 
-   
+    # are all of the fields in our first re
     first_record = all_records[0]
     assert all(isinstance(one_field, str)
                for one_field in first_record)

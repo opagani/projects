3dc6353af288b67d2a20d8761bd570c01c8b624a
Reuven Lerner <reuven@lerner.co.il>
2021-10-17 21:00:55 +0300

    gitautosync update 121

--- rmlaparse/rmlaparse/aparser.py
+++ rmlaparse/rmlaparse/aparser.py
@@ -2,6 +2,11 @@ import csv
 
 
 class ApacheParse:
+    _RECORD_FIELDS = ['ip_address', 'auth_info1', 'auth_info2',
+                      'timestamp_main_part', 'timestamp_tz', 'request',
+                      'response_code', 'bytes_returned', 'referrer',
+                      'user_agent']
+
     def __init__(self, filename):
         self.f = open(filename)
 
@@ -9,3 +14,8 @@ class ApacheParse:
         r = csv.reader(self.f, delimiter=' ')
         for one_line in r:
             yield one_line
+
+    def record_dicts(self):
+        for one_record in self.parse_file():
+            yield dict(zip(ApacheParse._RECORD_FIELDS,
+                           one_record))

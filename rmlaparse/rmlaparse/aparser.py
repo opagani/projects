0103337d933002a906bc5274d974c32db8965bf7
Reuven Lerner <reuven@lerner.co.il>
2021-10-17 19:48:13 +0300

    gitautosync update 48

--- rmlaparse/rmlaparse/aparser.py
+++ rmlaparse/rmlaparse/aparser.py
@@ -1,3 +1,11 @@
+import csv
+
+
 class ApacheParse:
     def __init__(self, filename):
         self.f = open(filename)
+
+    def parse_file(self):
+        r = csv.reader(self.f)
+        for one_line in r:
+            yield one_line

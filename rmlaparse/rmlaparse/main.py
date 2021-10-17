9953fb555c6db4954dae95b25d4baac40960d496
Reuven Lerner <reuven@lerner.co.il>
2021-10-17 21:16:47 +0300

    gitautosync update 143

---
+++ rmlaparse/rmlaparse/main.py
@@ -0,0 +1,8 @@
+from rmlaparse.aparser import ApacheParse
+
+
+def run():
+    ap = ApacheParse(
+        '/Users/reuven/Courses/Current/lerner-2021-10oct-17/rmlaparse/tests/access.log.1')
+    for one_record in ap.record_dicts():
+        print(one_record)

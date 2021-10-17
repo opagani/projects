6bd3cab1e4c6f4b9acdb338875aa26f5ef145bfe
Reuven Lerner <reuven@lerner.co.il>
2021-10-17 21:22:09 +0300

    gitautosync update 148

---
+++ rmlaparse/rmlaparse/flycheck_main.py
@@ -0,0 +1,12 @@
+
+from rmlaparse import aparser
+
+
+def run():
+    ap = aparser.ApacheParse(
+        '/Users/reuven/Courses/Current/lerner-2021-10oct-17/rmlaparse/tests/access.log.1')
+    for one_record in ap.record_dicts():
+        print(one_record)
+
+
+run()

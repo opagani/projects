137d2c1928e47754f84265c0666dd52a929411ed
Reuven Lerner <reuven@lerner.co.il>
2021-10-17 21:30:37 +0300

    gitautosync update 169

--- rmlaparse/rmlaparse/main.py
+++ rmlaparse/rmlaparse/main.py
@@ -1,8 +1,9 @@
 from rmlaparse import aparser
+import sys
 
 
 def run():
-    ap = aparser.ApacheParse(
-        '/Users/reuven/Courses/Current/lerner-2021-10oct-17/rmlaparse/tests/access.log.1')
+    filename = sys.args[1]
+    ap = aparser.ApacheParse(filename)
     for one_record in ap.record_dicts():
         print(one_record)

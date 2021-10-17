9d88269f6c23cb31a402d9f270a631ffcb3ce62f
Reuven Lerner <reuven@lerner.co.il>
2021-10-17 20:10:03 +0300

    gitautosync update 56

--- rmlaparse/tests/test_rmlaparse.py
+++ rmlaparse/tests/test_rmlaparse.py
@@ -4,3 +4,8 @@ from rmlaparse.aparser import ApacheParse
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+def test_create_instance():
+    ap = ApacheParse('/etc/passwd')
+    assert isinstance(ap, ApacheParse)
